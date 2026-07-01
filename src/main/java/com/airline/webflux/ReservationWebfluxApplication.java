91273ebcb26bb8738807d5007ac701fd3502b422
eduardomontachana305-del <eduardomontachana305@gmail.com>
2026-06-30 22:00:39 -0500

    chore: inicializa proyecto spring webflux

---
+++ src/main/java/com/airline/webflux/ReservationWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.airline.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationWebfluxApplication.class, args);
+    }
+
+}
